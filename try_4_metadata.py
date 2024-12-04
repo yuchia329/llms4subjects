868e4fb628afddb0cede02c688b5e2651b70afb6
yuchia329 <yuchia.chang@outlook.com>
2024-12-03 21:39:05 -0800

    clean try_4_metadata

--- try_4_metadata.py
+++ try_4_metadata.py
@@ -21,13 +21,6 @@ def get_subject_metadata(unique_label_set):
         category_subject_mapping = {}
         for label in unique_label_set:
             label_metadata = label_mapping.get(label)
-            # core_label_metadata = (
-            #     label_metadata.get("Name")
-            #     + " " + label_metadata.get("Classification Name")
-            #     + " " + " ".join(label_metadata.get("Alternate Name", []))
-            #     + " " + " ".join(label_metadata.get("Related Subjects", []))
-            #     + " " + label_metadata.get("Definition","")
-            # )
             if category_subject_mapping.get(label) is None:
                 category_subject_mapping[label_metadata["Classification Name"]] = [label]
             else:
@@ -42,7 +35,6 @@ def get_subject_metadata(unique_label_set):
                 "Definition": label_metadata.get("Definition",""),
             }
             subject_metadata_mapping[label] = core_label_metadata
-        # print('subject_description_mapping: ', subject_description_mapping)
         return subject_metadata_mapping, category_subject_mapping
 
 def generateLabelMetadata(unique_label_set):
@@ -64,14 +56,6 @@ def generateLabelMetadata(unique_label_set):
                 "Definition": label_metadata.get("Definition",""),
             }
             subjects_metadata.append(core_label_metadata)
-    # subjects_metadata = [
-    #     {
-    #         "Code": "gnd:4171549-4",
-    #         "Name": "Neuerwerbungsliste",
-    #         "Classification Name": "Bibliothek, Information und Dokumentation",
-    #         "Related Subjects": ["Verzeichnis", "Katalog", "Bibliografie"]
-    #     }
-    # ]
 
     # Create embeddings
     subject_embeddings = {}
@@ -86,4 +70,3 @@ def generateLabelMetadata(unique_label_set):
         subject_embeddings[subject["Code"]] = embedder.encode(metadata_text)
 
     return subject_embeddings
-# Save embeddings for later use
